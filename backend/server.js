07f598cda8e9bce1b816bb7681b871d65d447ea4
anand0265 <156602937+anand0265@users.noreply.github.com>
2025-07-08 19:51:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 // }));
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://inventory-mern-oh02.onrender.com/',
+  'https://inventory-mern-frontend.onrender.com/',
 ];
 
 app.use(cors({
