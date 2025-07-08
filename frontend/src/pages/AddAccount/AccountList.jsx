43b0fc4cffa73472a2b5fef1918df2cb9dcd335b
anand0265 <156602937+anand0265@users.noreply.github.com>
2025-07-08 18:28:49 +0530

    Update AccountList.jsx

--- frontend/src/pages/AddAccount/AccountList.jsx
+++ frontend/src/pages/AddAccount/AccountList.jsx
@@ -23,7 +23,7 @@ const ProductList = () => {
 
   const fetchProducts = async () => {
     try {
-      const res = await axios.get('http://localhost:5000/api/account/');
+      const res = await axios.get('https://inventory-mern-oh02.onrender.com/api/account/');
       setProducts(res.data.accounts);
       setFiltered(res.data.accounts);
     } catch (err) {
@@ -36,7 +36,7 @@ const ProductList = () => {
     if (!confirm) return;
 
     try {
-      await axios.delete(`http://localhost:5000/api/account/delete/${productId}`);
+      await axios.delete(`https://inventory-mern-oh02.onrender.com/api/account/delete/${productId}`);
       toast.success('Deleted successfully');
       fetchProducts();
     } catch (error) {
