384c91901835ef241d61f401121cf6d462aa19a5
anand0265 <156602937+anand0265@users.noreply.github.com>
2025-07-08 18:38:35 +0530

    Update Login.jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://localhost:5000/api/users/login', {
+      const res = await axios.post('https://inventory-mern-oh02.onrender.com/api/users/login', {
         email,
         password,
       });
