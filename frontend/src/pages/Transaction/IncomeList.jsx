bbd760702bb84f9325e22a0c64081930737320b5
anand0265 <156602937+anand0265@users.noreply.github.com>
2025-07-08 18:56:28 +0530

    Update IncomeList.jsx

--- frontend/src/pages/Transaction/IncomeList.jsx
+++ frontend/src/pages/Transaction/IncomeList.jsx
@@ -23,7 +23,7 @@ const IncomeList = () => {
 
   const fetchProducts = async () => {
     try {
-      const res = await axios.get('http://localhost:5000/api/re-income/');
+      const res = await axios.get('https://inventory-mern-oh02.onrender.com/api/re-income/');
       setProducts(res.data);
       setFiltered(res.data);
     } catch (err) {
@@ -36,7 +36,7 @@ const IncomeList = () => {
     if (!confirm) return;
 
     try {
-      await axios.delete(`http://localhost:5000/api/re-income/delete/${productId}`);
+      await axios.delete(`https://inventory-mern-oh02.onrender.com/api/re-income/delete/${productId}`);
       toast.success('Deleted successfully');
       fetchProducts();
     } catch (error) {
