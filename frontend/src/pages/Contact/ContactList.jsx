3b99eb1f464d01d4fe22a73a559f486d26db3fe9
anand0265 <156602937+anand0265@users.noreply.github.com>
2025-07-08 18:32:22 +0530

    Update ContactList.jsx

--- frontend/src/pages/Contact/ContactList.jsx
+++ frontend/src/pages/Contact/ContactList.jsx
@@ -23,7 +23,7 @@ const ContactList = () => {
 
   const fetchProducts = async () => {
     try {
-      const res = await axios.get('http://localhost:5000/api/contacts/');
+      const res = await axios.get('https://inventory-mern-oh02.onrender.com/api/contacts/');
       setProducts(res.data);
       setFiltered(res.data);
     } catch (err) {
@@ -36,7 +36,7 @@ const ContactList = () => {
     if (!confirm) return;
 
     try {
-        await axios.delete(`http://localhost:5000/api/contacts/delete/${contactId}`);
+        await axios.delete(`https://inventory-mern-oh02.onrender.com/api/contacts/delete/${contactId}`);
       toast.success('Deleted successfully');
       fetchProducts();
     } catch (error) {
